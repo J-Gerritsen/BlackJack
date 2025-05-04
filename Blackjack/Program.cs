cfc0feca63ac640d6a20a690ebbda548820a12be
job-gerritsen <154206850+J-Gerritsen@users.noreply.github.com>
2025-05-04 23:27:02 +0200

    Moved the game logic from program.cs to Game.cs

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -15,52 +15,8 @@ namespace Blackjack
             // see https://aka.ms/applicationconfiguration.
             //ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
-
-            Deck deck = new Deck();
-            deck.PrintDeck();
-            Console.WriteLine("\n");
-            deck.Shuffle();
-            deck.PrintDeck();
-            Player player1 = new Player();
-            player1.Hit(deck.DrawCard());
-            player1.Hit(deck.DrawCard());
-
-            while (true)
-                {
-                    player1.ShowHand();
-
-                int total = player1.GetTotalValue();
-
-                if (player1.IsBust)
-                {
-                    Console.WriteLine("You are busted");
-                    break;
-                }
-
-                if (player1.HasBlackjack)
-                {
-                    Console.WriteLine("Blackjack");
-                    break;
-                }
-
-                Console.Write("Do you want to Hit or Stand? ");
-                string input = Console.ReadLine().ToUpper();
-
-                if (input == "HIT")
-                {
-                    player1.Hit(deck.DrawCard());
-                }
-                else if (input == "STAND")
-                {
-                    player1.Stand();
-                    Console.WriteLine("You chose stand.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("choose either one listed above");
-                }
-            }
+            Game game = new Game();
+            game.Start();
         }
     }
 }

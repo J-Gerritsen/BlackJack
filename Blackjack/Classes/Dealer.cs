22c0ac61317fea2c7babe4fe544afeb3fe8fe73b
job-gerritsen <154206850+J-Gerritsen@users.noreply.github.com>
2025-05-04 23:13:46 +0200

    Added the dealer class

---
+++ Blackjack/Classes/Dealer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Classes
+{
+    class Dealer
+    {
+        private Hand hand = new Hand();
+
+        bool hasStood = false;
+
+        public bool HasBlackjack => hand.CalculateValue() == 21;
+        public bool IsBust => hand.CalculateValue() > 21;
+
+
+        public void Hit(Card card)
+        {
+            if (!hasStood)
+                hand.AddCard(card);
+        }
+
+        public void Stand()
+        {
+            hasStood = true;
+        }
+
+        public int GetTotalValue()
+        {
+            return hand.CalculateValue();
+        }
+
+        public void ShowHand()
+        {
+            hand.ShowHand("Dealer");
+        }
+
+    }
+}
